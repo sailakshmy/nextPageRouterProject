ed2e455a66e39fab6f88f32618e5eeca96fc0a14
sailakshmy <sailakshmy94@gmail.com>
2025-03-03 06:37:28 +0400

    Used the Link component statically and dynamically

--- nextjs-course-code-01-getting-started-starting-project/pages/clients/index.js
+++ nextjs-course-code-01-getting-started-starting-project/pages/clients/index.js
@@ -1,9 +1,36 @@
+import Link from "next/link";
 import React from "react";
 
 const ClientPage = () => {
+  const clients = [
+    {
+      id: "Max",
+      name: "Maximus",
+    },
+    {
+      id: "Min",
+      name: "Minimus",
+    },
+  ];
   return (
     <div>
       <h1>This is the Client page</h1>
+      <ul>
+        {clients?.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: {
+                  id: client.id,
+                },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- nextjs-course-code-01-getting-started-starting-project/pages/index.js
+++ nextjs-course-code-01-getting-started-starting-project/pages/index.js
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1> The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 };
