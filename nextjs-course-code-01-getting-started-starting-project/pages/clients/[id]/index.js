b6ce42d7076ca574a73d0c7c6733e6dad31af541
sailakshmy <sailakshmy94@gmail.com>
2025-03-03 06:43:52 +0400

    Navigating via router

--- nextjs-course-code-01-getting-started-starting-project/pages/clients/[id]/index.js
+++ nextjs-course-code-01-getting-started-starting-project/pages/clients/[id]/index.js
@@ -4,9 +4,20 @@ import React from "react";
 const ClientProjectsPage = () => {
   const router = useRouter();
   console.log(router.query);
+  function loadProjectHandler() {
+    // router.push(`/clients/${router.query.id}/projectA`);
+    router.push({
+      pathname: "/clients/[id]/[clientProjectId]",
+      query: {
+        id: router.query.id,
+        clientProjectId: "projectB",
+      },
+    });
+  }
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load the project</button>
     </div>
   );
 };
